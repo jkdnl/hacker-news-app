ce2d9e60a5a3561306cca943cd7f6ae24e0d8408
Daniil Iakovlev <ydaniil29@gmail.com>
2022-11-13 01:43:57 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar: React.FC = () => {
+    return (
+        <div className="bg-neutral-800 h-[10vh] flex justify-center">
+            <h1 className="my-auto text-lg font-bold tracking-widest">Hacker News</h1>
+        </div>
+    );
+};
+
+export default NavBar;
